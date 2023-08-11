211cd198023bc592c0f53dd7581ac4d5e9edc3fb
James Petrie <jamesppetrie@hotmail.com>
2023-08-11 21:29:59 +1000

    Basic interfaces

---
+++ src/filter.ts
@@ -0,0 +1,10 @@
+import { 
+  Branch,
+  dataStore,
+  ColumnIndex
+} from './interfaces'
+
+export function FilterData(data: dataStore, branch: Branch, owePeriod: number, balMin: number, balMax: number, exclude: string[]) {
+  data
+
+}
---
+++ src/interfaces.ts
@@ -0,0 +1,28 @@
+export enum Branch {
+  All,
+  Mudgee,
+  Orange,
+  Bathurst,
+  Taree,
+  Gunnedah,
+  PortMacquarie,
+  CoffsHarbour,
+  Dubbo,
+  Global,
+}
+
+export enum ColumnIndex {
+  id,
+  firstName,
+  lastName,
+  email,
+  owe30,
+  owe60,
+  owe90,
+  branch
+}
+
+
+export interface dataStore {
+
+}
